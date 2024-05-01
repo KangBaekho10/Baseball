e8307370a422947a097ce9ee0905ece4d2b2df36
KangBaekho <xodhks6740@gmail.com>
2024-05-01 19:15:23 +0900

    Feat: 게임 기록 보기 및 오류 메세지 추가

---
+++ src/main/kotlin/BullsAndCows.kt
@@ -0,0 +1,114 @@
+package org.example
+
+class StartGame() {
+    var a = 1
+    val array = Array<String>(50) { ' '.toString() }
+
+    fun getMenu(): Int {
+        println()
+        println("환영합니다! 원하시는 번호를 입력해주세요!")
+        println("1. 게임 시작하기 | 2. 게임 종료하기")
+        print(": ")
+        val userInput = readln().toInt()
+
+        when (userInput) {
+            1 -> startGame()
+            2 -> endGame()
+        }
+
+        if (userInput < 1 || userInput > 3) {
+            println("올바른 숫자를 입력해주세요!")
+            return getMenu()
+        }
+        return userInput
+    }
+
+    fun randomInt(): MutableSet<Int> {
+        val answer = mutableSetOf<Int>(0, 0, 0)
+        while (answer.size < 3) {
+            val randomInt = (0..9).random()
+            answer.add(randomInt)
+            if (answer.toList()[0] == 0) {
+                answer.remove(randomInt)
+            } else {
+                continue
+            }
+        }
+        return answer
+    }
+
+    fun startGame(): Any {
+        var i = 1
+        val answer = randomInt()
+        println()
+        println("< 게임을 시작합니다 >")
+        // println(answer) // 테스트용 정답코드 출력
+        val correctAnswer = answer.toString().toList().filter { it.isDigit() }
+        while (i <= 9) {
+            i++
+            println()
+            println("숫자를 입력하세요")
+            print(": ")
+            val inputAnswer = readln().toList()
+            if (inputAnswer.size > 3) {
+                println("올바른 숫자를 입력해주세요!")
+                i--
+                continue
+            }
+
+            val score = mutableListOf<Int>(0, 0, 0)
+
+            if (inputAnswer == correctAnswer) {
+                i -= 1
+                println()
+                println("$answer 정답입니다. (${i}회 시도)")
+                array[a] = "$i"
+                a++
+                break
+            } else {
+                if (i < 10) {
+                    correctAnswer.forEachIndexed { index, count ->
+                        val point = inputAnswer.indexOf(count)
+                        when {
+                            point == index -> ++score[0]
+                            point != -1 -> ++score[1]
+                            else -> ++score[2]
+                        }
+                    }
+                    println("${score[0]} 스트라이크 | ${score[1]} 볼 | ${score[2]} 아웃")
+                    continue
+                } else {
+                    i -= 1
+                    println()
+                    println("게임 오버!")
+                    println("정답은? : $correctAnswer")
+                    array[a] = "$i"
+                    a++
+                }
+            }
+        }
+        // println(array.contentToString())
+        while (true) {
+            println()
+            println("게임을 계속 할까요? ('1' : Yes / '2' : No - 기록 보기)")
+            print(": ")
+            val retry: String = readln()
+            return if (retry == "1") {
+                startGame()
+            } else if (retry == "2") {
+                println("< 게임 기록 보기 >")
+                val records = array
+                for (trying in 0..9) {
+                    val record: String = records[trying]
+                    if (record != " ") {
+                        println("$trying 번째 게임 , 시도 횟수 : $record")
+                    }
+                }
+                getMenu()
+            } else {
+                println("올바른 숫자를 입력해주세요!")
+                continue
+            }
+        }
+    }
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,118 +1,21 @@
 package org.example
 
-//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
-// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
+
 fun main() {
     var isValid = false
-    var selectedMenu = 0
 
     while (!isValid) {
         try {
-            selectedMenu = getMenu()
+            StartGame().getMenu()
             isValid = true
         } catch (e: Exception) {
             isValid = false
         }
     }
-
-    when (selectedMenu) {
-        1 -> startGame()
-        2 -> endGame()
-        3 -> gameInfo()
-    }
-}
-
-fun getMenu(): Int {
-    println()
-    println("환영합니다! 원하시는 번호를 입력해주세요!")
-    println("1. 게임 시작하기 | 2. 게임 종료하기 | 3. 게임 기록 보기")
-    print(": ")
-    val userInput = readln().toInt()
-
-    if (userInput < 1 || userInput > 3) {
-        println("올바른 숫자를 입력해주세요!")
-        return getMenu()
-    }
-    return userInput
-}
-
-fun startGame(): Any {
-    val recordList: MutableList<Map<Int, Int>> = mutableListOf()
-    val num1 = 0
-    val keys = 0
-    val values = 0
-    val answer = mutableSetOf<Int>(0, 0, 0)
-    while (answer.size < 3) {
-        val randomInt = (0..9).random()
-        answer.add(randomInt)
-        if (answer.toList()[0] == 0) {
-            answer.remove(randomInt)
-        } else {
-            continue
-        }
-    }
-    println()
-    println("< 게임을 시작합니다 >")
-    // println(answer) 테스트용 정답
-    val correctAnswer = answer.toString().toList().filter { it.isDigit() }
-    for (i in 1..9) {
-        println()
-        println("숫자를 입력하세요")
-        print(": ")
-        val inputAnswer = readln().toList()
-
-        val score = mutableListOf<Int>(0, 0, 0)
-
-        if (inputAnswer == correctAnswer) {
-            println("$answer 정답입니다. (${i}회 시도)")
-            val record = mapOf(keys to num1, values to i)
-            recordList.add(record)
-            break
-        } else {
-            if (i < 9) {
-                correctAnswer.forEachIndexed { index, count ->
-                    val point = inputAnswer.indexOf(count)
-                    when {
-                        point == index -> ++score[0]
-                        point != -1 -> ++score[1]
-                        else -> ++score[2]
-                    }
-                }
-                println("${score[0]} 스트라이크 | ${score[1]} 볼 | ${score[2]} 아웃")
-                continue
-            } else {
-                println("게임 오버!")
-                println("정답은? : $correctAnswer")
-            }
-        }
-    }
-    println("게임을 다시 할까요? ('1' : Yes / '2' : No)")
-    print(": ")
-    val retry: String = readln()
-    return if (retry == "1") {
-        startGame()
-    } else {
-        main()
-    }
 }
 
 
-fun endGame() {
-    println("< 숫자 야구 게임을 종료합니다 >")
-    return
-}
-
-
-fun gameInfo() {
-//        var trying = 0
-//        val gamerecordList = StartGame.recordList()
-//        println("< 게임 기록 보기 >")
-//        println("")
-//        for (recording in recordList) {
-//            ++trying
-//            recording.forEach { println("$trying 번째 게임 : 시도 횟수 - ${it.value}회") }
-//        }
-//    기록 저장 수정 예정
-}
-
-
+    fun endGame() {
+        println("< 숫자 야구 게임을 종료합니다 >")
+        return
+    }
